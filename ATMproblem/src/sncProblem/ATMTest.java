3a4162339f524d4f6cc09c9bd8e52e58b3c1c42b
Andrew Kim <19kima@elmbrookstudents.org>
2018-02-21 22:46:31 -0600

    Test Class initialized

---
+++ ATMproblem/src/sncProblem/ATMTest.java
@@ -0,0 +1,58 @@
+package sncProblem;
+
+import java.util.Scanner;
+
+public class ATMTest {
+
+	public static void main(String args[]) {
+		String fullCommand;
+		String command;
+		String number;
+		int quit = 1;
+		Scanner input = new Scanner(System.in);
+		ATM eric = new ATM();
+
+		do {
+			fullCommand = input.nextLine();
+			command = Character.toString((fullCommand.toLowerCase()).charAt(0));
+			switch (command) {
+			case "r":
+				eric.refill();
+				break;
+			case "w":
+				number = fullCommand.substring(fullCommand.indexOf("$") + 1);
+				int take = Integer.parseInt(number);
+				eric.take(take);
+				break;
+			case "i":
+				number = fullCommand + " ";
+				if (number.contains(" $100 ")) {
+					System.out.println("$100 - " + eric.getHundreds());
+				}
+				if (number.contains(" $50 ")) {
+					System.out.println("$50 - " + eric.getFifty());
+				}
+				if (number.contains(" $20 ")) {
+					System.out.println("$20 - " + eric.getTwenty());
+				}
+				if (number.contains(" $10 ")) {
+					System.out.println("$10 - " + eric.getTens());
+				}
+				if (number.contains(" $5 ")) {
+					System.out.println("$5 - " + eric.getFives());
+				}
+				if (number.contains(" $1 ")) {
+					System.out.println("$1 - " + eric.getOnes());
+				}
+				break;
+			case "q":
+				quit = 0;
+				break;
+			default:
+				System.out.println("Failure: Invalid Command");
+				break;
+			}
+		} while (quit == 1);
+
+	}
+}
